8eb5b6abc8e10143b19d151f3957e83646b01efb
Zohirul Alam Tiemoon <TOSHIBA@TOSHIBA-PC>
2014-02-22 17:18:36 +0600

    new file category added.

---
+++ category.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of category
+ *
+ * @author TOSHIBA
+ */
+class category {
+    public  $code;
+    public  $name;
+}
